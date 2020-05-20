3c7ac0a3abaa5b1db7aaf358e1250e798f32a3d9
evan <aspaasevan@gmail.com>
2020-05-20 12:14:30 -0700

    test file setup'

---
+++ enhancing/__tests__/enhancer.spec.js
@@ -0,0 +1,10 @@
+const enhancer = require('../enhancer.js');
+// test away!
+describe("enhancer", () => {
+  it('should succeed', () =>{
+
+  });
+  it.todo('should fail');
+  it.todo('should repair something');
+  it.todo('should get something');
+})
