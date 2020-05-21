d8f26ce8172fd13090fd05fdd7afbef183d31b03
evan <aspaasevan@gmail.com>
2020-05-20 18:54:05 -0700

    succeed adds 1 to enhancement

--- enhancing/__tests__/enhancer.spec.js
+++ enhancing/__tests__/enhancer.spec.js
@@ -2,17 +2,23 @@ const {succeed, fail, repair, get} = require('../enhancer.js');
 
 // test away!
 describe("enhancer", () => {
-  it('should repair something', () => {
+  it('should repair durability to 100', () => {
     const item = {
       name: 'foo', 
       enhancement: 1, 
       durability: 50
     };
-    const repairedItem = {...item, durability: 100};
-    expect(repair(item)).toEqual(repairedItem);
+    expect(repair(item)).toEqual({...item, durability:100});
   });
 
-  // it.todo('should succeed');
+  it('should succeed', () => {
+    const item2 = { 
+      name: 'foobar', 
+      enhancement: 1,
+    };
+    item2.enhancement += 1;
+    expect(succeed(item2)).toEqual({...item2})
+  });
   // it.todo('should fail');
   // it.todo('should get something');
 })
--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -7,16 +7,17 @@ module.exports = {
 
 const item = {
   name: 'gamerdude',
-  enhancement: 1 ,
+  enhancement: 1,
   durability: 50
 };
 
-// - Items have `name`, `durability` and `enhancement`.
-// - The item's `enhancement` it's a number from 0 to 20.
-// - The item's `durability` it's a number from 0 to 100.
 
 function succeed(item) {
-  return { ...item };
+  // - If the item enhancement level is 20, the enhancement level is not changed.
+  // - The durability of the item is not changed.
+  item.enhancement += 1;
+
+  return {...item};
 }
 
 function fail(item) {
@@ -27,7 +28,7 @@ function repair(item) {
   return { 
     ...item,
     durability: 100
-   };
+  };
 }
 
 function get(item) {
